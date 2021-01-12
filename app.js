25acc6d984c025facb6ac5fc59c18ac93c9d59aa
RupaliThenge <rupalivt@gmail.com>
2021-01-12 12:04:05 -0800

    changed the userRoutes path to remove error

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const userRoutes = require('../nodeauthapi/routes/user');
+//const userRoutes = require('../nodeauthapi/routes/user');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb://sa:admin@node-rest-counter-shard-00-00-b3njp.mongodb.net:27017,node-rest-counter-shard-00-01-b3njp.mongodb.net:27017,node-rest-counter-shard-00-02-b3njp.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-counter-shard-0&authSource=admin&retryWrites=true')
 
